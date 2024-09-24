3e6d664fd3fcd90f44f0317bec121c5e4828dc9f
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-24 22:03:03 +0200

    change: add more flexibility to APIException

--- pytwitter/errors.py
+++ pytwitter/errors.py
@@ -4,6 +4,6 @@ class TwitterAPIException(Exception):
     """
     Exception when request does not return a status code 2xx
     """
-    def __init__(self, status_code, message: str = ""):
-        super().__init__(f"Request failed with code {status_code}. {message}")
+    def __init__(self, message: str = ""):
+        super().__init__(f"{message}")
 
